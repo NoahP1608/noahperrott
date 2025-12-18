8206d4cc4dc572983efbcf4ce46258f79e272ad6
NoahP1608 <58424626+NoahP1608@users.noreply.github.com>
2025-12-18 20:50:13 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function setcolor(){
+  let x = document.getElementsByTagName('BODY')[0];
+  x.style.backgroundColor = 'white'
+}
+
+function changecolor(color) {
+  let x = document.getElementsByTagName('BODY')[0];
+  x.style.backgroundColor = color;
+}
